83d49a69c955f52efdbc27f5635ffd2465268d88
Wess Cope <wcope@me.com>
2012-09-19 11:46:19 -0400

    Initial Commit

---
+++ Vendor/VAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  VAppDelegate.h
+//  Vendor
+//
+//  Created by Wess Cope on 9/19/12.
+//  Copyright (c) 2012 Wess Cope. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Vendor/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Vendor
+//
+//  Created by Wess Cope on 9/19/12.
+//  Copyright (c) 2012 Wess Cope. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "VAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([VAppDelegate class]));
+    }
+}
